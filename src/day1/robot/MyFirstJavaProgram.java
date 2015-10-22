a2a6f5750696f4973f68ce710c0b8e78c7933b22
JamesBusby <Guest@wts8.wts.local>
2015-10-21 18:30:07 -0700

    aria is a potatooooooooo!!!

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -4,12 +4,22 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	public static void main(String[] args) {
-		
-		// START HERE
-
+	 Robot steve = new Robot("batman");
 	
-		
-		
-	}
+	
+	 public static void main(String[] args) {
+		 
+			for (int i = 0; i < 4; i++) {
+	
+steve.setRandomPenColor();
+ steve.penDown();
+ steve.sparkle();
+ steve.setSpeed(5);
+steve.move(100);
+steve.turn(90);
+
+}
+
+
+}
 }
