4c551e8a67686fb79ef4d696aa7f0d233a251bca
JamesBusby <Guest@WTS8.wts.local>
2015-11-04 18:26:59 -0800

    magic 8 ball

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -15,17 +15,44 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
-		// 2. Catch the user's answer in a String
-
+       speak("Spell ariaariaariaariaariaaria  is a alllluuumanati");
+	    // 2. Catch the user's answer in a String
+       String spell = JOptionPane.showInputDialog("");
 		// 3. If the user spelled the word correctly, speak "correct"
-
-		// 4. Otherwise say "wrong"
-
-		// 5. repeat the process for other words
-		
+        if (spell.equals("mandlebrot")){
+        speak("aria is a pie stuffed in a chiken on top of a potaoe lima bean pie");
 	}
-
+		// 4. Otherwise say "wrong"
+        else {
+        	speak("you are a lima bean");
+        }
+		// 5. repeat the process for other words
+	   speak("aria ate the alumanati  and threw conrad in a pie");
+	    // 2. Catch the user's answer in a String
+      String other = JOptionPane.showInputDialog("");
+		// 3. If the user spelled the word correctly, speak "correct"
+       if (other.equals("conrad is a comquat")){
+       speak("yes he is");
+	}
+		// 4. Otherwise say "wrong"
+       else {
+       	speak("you are a comquat");
+       }
+	
+	
+	   speak("Spell conrad and aria are both lima bean comquat pie stew");
+	    // 2. Catch the user's answer in a String
+      String also = JOptionPane.showInputDialog("");
+		// 3. If the user spelled the word correctly, speak "correct"
+       if (also.equals("conrad and aria are both lima bean comquat pie stew")){
+       speak("the computer has spoken and it cannot be denied");
+	}
+		// 4. Otherwise say "wrong"
+       else {
+       	speak("everybody knows that aria's   is    a    glass of noodles  COMPUTERS NEVER LIE!!!    and a picle in a pie on top of a glass of noodles"
+       			+ "     alumanati ");
+       }
+	}
 	static void speak(String words) {
 		try {
 			Runtime.getRuntime().exec("say " + words).waitFor();
